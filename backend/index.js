e8b4e3f1e926d243207fdb76a6e46a1ca4f38e7d
Sajid788 <mdsajidalam8340@gmail.com>
2025-02-11 15:08:02 +0530

    Register and login routes done

---
+++ backend/index.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const cors = require("cors");
+const { connection, PORT } = require("./config/db");
+const { userRouter } = require("./routes/UserRoutes");
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+
+app.get("/", (req, res) => {
+    res.json({ message: "Server is running!"});
+});
+
+app.use("/api/users", userRouter);
+
+
+app.listen(PORT, async () => {
+  try {
+    await connection;
+    console.log("Connected to DataBase");
+  } catch (error) {
+    console.log(`${error} is giving while connecting`);
+  }
+  console.log(`Listening on PORT: ${PORT}`);
+});
